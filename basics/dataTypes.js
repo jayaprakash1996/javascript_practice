0aa780d7a5396f35dcfe73b7353c937650c256bc
jayaprakash1996 <90298693+jayaprakash1996@users.noreply.github.com>
2023-11-26 15:51:47 +0000

    data types added

---
+++ basics/dataTypes.js
@@ -0,0 +1,19 @@
+"use strict" //treat all js code as newer version
+
+// alert("hello") //this will not work as we are using node.js and not a browser
+
+// premetive datatypes
+
+// number=> 2
+// bigint=> any bigger number
+// string=>""
+// boolean=>"true/false"
+// null=>standalone value
+// underfined=> var jay;
+// symbol=>unique
+
+// object
+
+// while checking type of this errors we will get this as a output
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
---
+++ basics/variables.js
@@ -0,0 +1,9 @@
+const names="jayaprakash"
+let age="28"
+var city="bbsr"
+
+city="bhubaneswar"
+// prefer not to use var beacause of issue in block scope and functional scope
+// {} is a scope
+
+console.table([names,age,city])
